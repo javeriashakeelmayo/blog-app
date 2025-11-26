8ab9252eefe76a4546616886b777ca5fe6352a6c
Javeria Shakeel <javeriashakeelmayo@gmail.com>
2025-11-26 12:30:37 +0500

    change FBV into CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,28 @@
+# from django.urls import path
+# from . import views
+# from django.contrib.auth import views as auth_views
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('add-blog/', views.add_blog, name='add_blog'),
+#     path('add-post/', views.add_post, name='add_post'),
+#     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+#     path('signup/', views.Signup, name='signup'),
+#     path('login/',auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+#     path('post/<slug:slug>/like/', views.like_post, name='like_post'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+# ]
+
+
+
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
+from .views import HomeView, AddBlogView, AddPostView, PostDetailView, SignupView, LikePostView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('add-blog/', views.add_blog, name='add_blog'),
-    path('add-post/', views.add_post, name='add_post'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('signup/', views.Signup, name='signup'),
-    path('login/',auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('post/<slug:slug>/like/', views.like_post, name='like_post'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('', HomeView.as_view(), name='home'),
+    path('add-blog/', AddBlogView.as_view(), name='add_blog'),
+    path('add-post/', AddPostView.as_view(), name='add_post'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('post/<slug:slug>/like/', LikePostView.as_view(), name='like_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,83 +1,84 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Blog, Post
-from .forms import BlogForm, PostForm, SignupForm, CommentForm
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.utils.decorators import method_decorator
+from .models import Blog, Post
+from .forms import BlogForm, PostForm, SignupForm, CommentForm
 
 
-def home(request):
-    blogs = Blog.objects.all()
-    return render(request, 'blog/home.html', {'blogs': blogs})
+
+class HomeView(ListView):
+    model = Blog
+    template_name = 'blog/home.html'
+    context_object_name = 'blogs'
 
 
-def add_blog(request):
-    if request.method == 'POST':
-        form = BlogForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = BlogForm()
-    return render(request, 'blog/add_blog.html', {'form': form})
+
+class AddBlogView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/add_blog.html'
+    success_url = reverse_lazy('home')
 
 
-def add_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = PostForm()
-    return render(request, 'blog/add_post.html', {'form': form})
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/add_post.html'
+    success_url = reverse_lazy('home')
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
 
-    comments = post.comments.all()
 
-    if request.method == "POST":
+class PostDetailView(View):
+    template_name = 'blog/post_detail.html'
+
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.all()
+        form = CommentForm()
+        return render(request, self.template_name, {'post': post, 'comments': comments, 'form': form})
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
             return redirect('post_detail', slug=slug)
-    else:
-        form = CommentForm()
-
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
+        comments = post.comments.all()
+        return render(request, self.template_name, {'post': post, 'comments': comments, 'form': form})
 
 
-def Signup(request):
-    if request.method == 'POST':
+
+class SignupView(View):
+    template_name = 'blog/signup.html'
+
+    def get(self, request):
+        form = SignupForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = SignupForm(request.POST)
-
         if form.is_valid():
-
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-
             login(request, user)
             return redirect('home')
-    else:
-        form = SignupForm()
-
-    return render(request, 'blog/signup.html', {'form': form})
-
-@login_required
-def like_post(request, slug):
-    post=get_object_or_404(Post, slug=slug)
-    if request.user in post.likes.all():
-        post.likes.remove(request.user)
-    else:
-        post.likes.add(request.user)
-    return redirect('post_detail', slug=slug)
+        return render(request, self.template_name, {'form': form})
 
 
 
-
-
-
+@method_decorator(login_required, name='dispatch')
+class LikePostView(View):
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        if request.user in post.likes.all():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        return redirect('post_detail', slug=slug)
